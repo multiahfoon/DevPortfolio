1b648858dfb48e7a858c86b42859c4a1099eca8f
multiahfoon <multiahfoon@gmail.com>
2021-07-12 12:43:03 +1200

    feat: remove login and signup

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -27,12 +27,8 @@ function Nav () {
             {currentUser ? <span className="flex relative order-1"><div className="flex items-center ml-auto"><button onClick={handleLogOut} className="flex items-center xl:ml-2 hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 px-3 lg:font-medium xl:font-semibold focus:outline-none lg:text-base">Log Out</button></div></span>
               : <span className="flex relative order-1">
                 <div className="flex items-center ml-auto">
-                  <Link to='/login'>
                     <button className="lg:font-medium xl:font-semibold mx-6 inline hover:text-blue-400 lg:text-base">Log in</button>
-                  </Link>
-                  <Link to='/signup'>
                     <button className="flex items-center xl:ml-2 hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 px-3 lg:font-medium xl:font-semibold focus:outline-none lg:text-base">Sign Up</button>
-                  </Link>
                 </div>
               </span>
             }
